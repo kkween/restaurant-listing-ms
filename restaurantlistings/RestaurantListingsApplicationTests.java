43b491b1f85f9467ad8fee2a965f36d9e895eb86
kkween <104529607+kkween@users.noreply.github.com>
2023-09-15 01:01:54 -0400

    Add files via upload

---
+++ restaurantlistings/RestaurantListingsApplicationTests.java
@@ -0,0 +1,9 @@
+package com.izooki.restaurantlistings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantListingsApplicationTests {
+
+}
